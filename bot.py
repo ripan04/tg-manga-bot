e40f9d339a139a756d894d67e44a757c36aa432e
ripan04 <112069582+ripan04@users.noreply.github.com>
2022-08-26 08:21:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,6 +115,10 @@ bot = Client('bot',
              api_hash=('c6f36f2887586704871201a0fea2e452'),
              bot_token=('5596236173:AAERUTzTFrXCiPb4ZHb3Ns_eAo-P1xADf8k'))
 
+@bot.on_message(filters=filters.command(['reboot']))
+async def on_refresh(client: Client, message: Message):
+    await message.reply("restarting......")
+    quit(1)
 
 @bot.on_message(filters=~(filters.private & filters.incoming))
 async def on_chat_or_channel_message(client: Client, message: Message):
